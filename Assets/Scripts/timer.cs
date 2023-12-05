1e66dbdc97d076cbb728df2b1b1952e4355077a8
Luciii0372 <143450514+Luciii0372@users.noreply.github.com>
2023-12-04 22:45:41 -0500

    timer
    timer

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float timeRemaining = 10; // Set the duration of the timer in seconds.
+    public bool timerIsRunning = false;
+    public Sprite[] clockHandSprites; // Array of sprites for each hand position.
+    public Image clockHandImage; // Reference to the Image component.
+
+    private float changeInterval;
+    private int currentSpriteIndex;
+
+    private void Start()
+    {
+        timerIsRunning = true;
+        changeInterval = timeRemaining / clockHandSprites.Length;
+        currentSpriteIndex = 0;
+    }
+
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+
+                // Change sprite at intervals.
+                if (timeRemaining <= changeInterval * (clockHandSprites.Length - 1 - currentSpriteIndex))
+                {
+                    clockHandImage.sprite = clockHandSprites[currentSpriteIndex];
+                    currentSpriteIndex++;
+                }
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                // When the timer ends, ensure the last sprite is shown.
+                clockHandImage.sprite = clockHandSprites[clockHandSprites.Length - 1];
+            }
+        }
+    }
+}
